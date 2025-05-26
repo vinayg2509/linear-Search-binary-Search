0c1a2b2f497169d212af999e67725fd20178fffe
vinayg2509 <vinayg2509@gmail.com>
2025-05-26 17:34:06 +0530

    [Vinay]Implement binary search to find the index of the smallest element (rotation point) in a rotated sorted array

--- src/main/java/com/bridgelabz/search/FindRotationPoint.java
+++ src/main/java/com/bridgelabz/search/FindRotationPoint.java
@@ -1,4 +1,40 @@
 package com.bridgelabz.search;
 
-public class FindRotationPoint {
+import java.util.Scanner;
+
+public class FindRotationPoint
+{
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter size of an array");
+        int size=scanner.nextInt();
+        int []a=new int[size];
+        System.out.println("Enter "+size+" element");
+        for (int i=0;i<size;i++)
+        {
+            a[i]=scanner.nextInt();
+        }
+        int index=findRotationIndex(a);
+        System.out.println(index);
+    }
+
+    private static int findRotationIndex(int[] a)
+    {
+
+        int left=0;
+        int right=a.length-1;
+        while(left<right)
+        {
+            int mid=(right+left)/2;
+            if(a[mid]>a[right])
+            {
+                left=mid+1;
+            }
+            if(a[mid]<a[right])
+            {
+                right=mid;
+            }
+        }
+        return left;
+    }
 }
